4290b28d833dfd9a629456d8ef392581270d1ead
SahrayaM <yeehawpartner121@gmail.com>
2021-04-21 21:54:27 +0000

    Yeehaw

--- Pong/index.js
+++ Pong/index.js
@@ -16,30 +16,30 @@ function runProgram(){
   "UP": 38,
   "DOWN": 40
 }
-
+///Ball variable///
 
   // Game Item Objects
   function factory($id){
   var revealInfo = {}
   revealInfo.id = $id;
-  revealInfo.x = $($id).css('left');
-  revealInfo.y = $($id).css('top');
+  revealInfo.x = parseFloat($($id).css('left'));
+  revealInfo.y = parseFloat($($id).css('top'));
   revealInfo.width = $($id).width();
   revealInfo.height = $($id).height();
   revealInfo.speedX = 0;
   revealInfo.speedY = 0;
- 
+
+
   return revealInfo;
 }
 
- top = parseFloat($($id).css('top'));
-
-left = parseFloat($($id).css('left'));
+ 
 
 ///Creating Variables///
 var leftPaddle = factory("#leftPaddle");
 var rightPaddle = factory("#rightPaddle");
 var ball = factory("#ball");
+var board = factory("#board");
 
 // Default speeds //
 ball.speedX = 5;
@@ -48,7 +48,7 @@ ball.speedX = 5;
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeydown);
-
+ $(document).on('keyup', handleKeyup);
    // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -66,6 +66,7 @@ ball.speedX = 5;
   updatePosition(ball);
   updatePosition(leftPaddle);
   updatePosition(rightPaddle);
+  ballCollision();
   }
   
   /* 
@@ -90,6 +91,47 @@ ball.speedX = 5;
        leftPaddle.speedY = 5;
    }
   }
+  ///Handle key up///
+ function handleKeyup(event) {
+    if (event.which === KEY.DOWN) {
+    rightPaddle.speedY = 0;
+    
+  }
+   
+  else if (event.which === KEY.UP) {
+    
+    rightPaddle.speedY= 0;
+  }
+ 
+   else if (event.which === KEY.W){
+       leftPaddle.speedY = 0;
+   }
+
+   else if (event.which === KEY.S){
+       leftPaddle.speedY = 0;
+   }
+  }
+
+  ///example///
+  
+  ///Ball Collision Function///
+function ballCollision(){
+    
+    
+    // TODO: Return true if they are overlapping, false otherwise
+ if (ball.y > rightPaddle.y){
+     ball.speedY *= -1;
+ }
+ else if (ball.x > rightPaddle.x){
+     ball.speedX *= -1;
+ }
+    else if (ball.y < leftPaddle.y){
+     ball.speedY *= -1;
+ }
+ else if (ball.x < leftPaddle.x){
+     ball.speedX *= -1;
+ }
+}
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -105,6 +147,12 @@ ball.speedX = 5;
         $(whichObject.id).css('top', whichObject.y);
 
     }
+    
+
+
+
+
+    
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
